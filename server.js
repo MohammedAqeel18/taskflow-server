83807e7314480f458225726e923132cc920db731
MohammedAqeel18 <aqeelworkmail@gmail.com>
2025-12-18 10:26:49 +0530

    feat: add express json middleware and check endpoint

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+require("dotenv").config();
 const express = require("express")
 
 const app = express();
 
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.get("/",(req,res)=>{
     res.send("TaskFlow Backend Running: Welcome to Taskflow server")
 })
 
+app.post("/test",(req,res)=>{
+    res.json({
+        recievedData:req.body,
+    })
+})
 app.listen(PORT,()=>{
     console.log("http://localhost:5000")
 })
